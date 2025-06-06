cf7f0a30a505cde5de4fcda2ebb6fc95d26fd4fd
Andreas-Theodoridis <andreas.theodoridis@oracle.com>
2025-06-07 01:54:12 +0300

    update create sql agent

--- other_scripts/create_ai_sql_agent.sh
+++ other_scripts/create_ai_sql_agent.sh
@@ -8,7 +8,8 @@ OBJECT_STRG_NS=$(awk -F': ' '/^object_strg_ns:/ {print $2}' "$CONFIG_FILE")
 BUCKET_NAME=$(awk -F': ' '/^bucket_name:/ {print $2}' "$CONFIG_FILE")
 AI_TOOL_SQL_SCHEMA=$(awk -F': ' '/^ai_agent_tool_sql_schema:/ {print $2}' "$CONFIG_FILE")
 AI_TOOL_ICL_EXAMPLES=$(awk -F': ' '/^ai_agent_tool_icl_examples:/ {print $2}' "$CONFIG_FILE")
-AI_TOOL_TABLE_DESCRIPTIONS=$(awk -F': ' '/^ai_agent_tool_table_descriptionss:/ {print $2}' "$CONFIG_FILE")
+AI_TOOL_TABLE_DESCRIPTIONS=$(awk -F': ' '/^ai_agent_tool_table_descriptions:/ {print $2}' "$CONFIG_FILE")
+
 
 # Step 1: Create the AI Agent and capture its OCID
 echo "Creating OCI AI Agent..."
@@ -25,11 +26,14 @@ if [[ -z "$AI_AGENT_JSON" ]]; then
 fi
 
 echo "AI Agent OCID: $AI_AGENT_JSON"
+echo "$AI_AGENT_JSON" > /home/opc/gen_ai_agent_id.txt
 
 # Step 2: Create the AI Agent endpoint
 oci generative-ai-agent agent-endpoint create \
     --agent-id "$AI_AGENT_JSON" \
-    --compartment-id "$COMPARTMENT_ID"
+    --compartment-id "$COMPARTMENT_ID" \
+    --display-name "Focus-Reports-SQL-Agent-Endpoint" \
+    --query 'data.id' --raw-output > /home/opc/gen_ai_agent_endpoint_id.txt
 
 # Step 3: Create OCI AI SQL Tool
 echo "Creating OCI AI SQL Tool..."
@@ -38,31 +42,33 @@ oci generative-ai-agent tool create-tool-sql-tool-config \
   --compartment-id "$COMPARTMENT_ID" \
   --display-name "SQLTool" \
   --description "Focus Reports AI SQL Agent Config" \
-  --tool-config-database-connection '{
-    "connectionId": "$DB_TOOLS_ID",
-    "connectionType": "DATABASE_TOOL_CONNECTION"
-  }' \
-  --tool-config-database-schema '{
-    "inputLocationType": "OBJECT_STORAGE_PREFIX",
-    "namespaceName": "$OBJECT_STRG_NS",
-    "bucketName": "$BUCKET_NAME",
-    "prefix": "$AI_TOOL_SQL_SCHEMA"
-  }' \
-  --tool-config-icl-examples '{
-    "inputLocationType": "OBJECT_STORAGE_PREFIX",
-    "namespaceName": "$OBJECT_STRG_NS",
-    "bucketName": "$BUCKET_NAME",
-    "prefix": "$AI_TOOL_ICL_EXAMPLES"
-  }' \
-  --tool-config-table-and-column-description '{
-    "inputLocationType": "OBJECT_STORAGE_PREFIX",
-    "namespaceName": "$OBJECT_STRG_NS}",
-    "bucketName": "$BUCKET_NAME",
-    "prefix": "$AI_TOOL_TABLE_DESCRIPTIONS"
-  }' \
+  --tool-config-database-connection "{
+    \"connectionId\": \"$DB_TOOLS_ID\",
+    \"connectionType\": \"DATABASE_TOOL_CONNECTION\"
+  }" \
+  --tool-config-database-schema "{
+    \"inputLocationType\": \"OBJECT_STORAGE_PREFIX\",
+    \"namespaceName\": \"$OBJECT_STRG_NS\",
+    \"bucketName\": \"$BUCKET_NAME\",
+    \"prefix\": \"$AI_TOOL_SQL_SCHEMA\"
+  }" \
+  --tool-config-icl-examples "{
+    \"inputLocationType\": \"OBJECT_STORAGE_PREFIX\",
+    \"namespaceName\": \"$OBJECT_STRG_NS\",
+    \"bucketName\": \"$BUCKET_NAME\",
+    \"prefix\": \"$AI_TOOL_ICL_EXAMPLES\"
+  }" \
+  --tool-config-table-and-column-description "{
+    \"inputLocationType\": \"OBJECT_STORAGE_PREFIX\",
+    \"namespaceName\": \"$OBJECT_STRG_NS\",
+    \"bucketName\": \"$BUCKET_NAME\",
+    \"prefix\": \"$AI_TOOL_TABLE_DESCRIPTIONS\"
+  }" \
   --tool-config-generation-llm-customization file:///home/opc/oci-focus-reports/config/llm_instruction.json \
   --tool-config-should-enable-sql-execution true \
   --tool-config-should-enable-self-correction true \
   --tool-config-dialect ORACLE_SQL \
   --tool-config-model-size LARGE \
-  --query 'data.id' --raw-output > /home/opc/sql_tool_ocid.txt"
+  --wait-for-state SUCCEEDED \
+  --wait-for-state FAILED \
+  --query 'data.id' --raw-output > /home/opc/sql_tool_ocid.txt
