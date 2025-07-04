8d6f8fe3a9df8dec203181b1d8894cba0b37a1b6
Andreas-Theodoridis <andreas.theodoridis@oracle.com>
2025-07-04 11:30:00 +0300

    update script

--- other_scripts/ov_update.sh
+++ other_scripts/ov_update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Prompt to run git_update.sh
-read -p "Do you want to run update files from gi? (yes/no): " answer1
-if [[ "$answer1" =~ ^[Yy][Ee][Ss]$ || "$answer1" =~ ^[Yy]$ ]]; then
+read -p "Do you want to run update files from git? (y/N): " answer1
+if [[ "${answer1,,}" == "y" ]]; then
   /home/opc/oci-focus-reports/other_scripts/git_update.sh
 else
   echo "Skipped git_update.sh"
 fi
 
 # Prompt to run db_apex_upgrade.py
-read -p "Do you want to update database and application? (yes/no): " answer2
-if [[ "$answer2" =~ ^[Yy][Ee][Ss]$ || "$answer2" =~ ^[Yy]$ ]]; then
+read -p "Do you want to update database and application? (y/N): " answer2
+if [[ "${answer2,,}" == "y" ]]; then
   /home/opc/oci-focus-reports/python_scripts/db_apex_upgrade.py
 else
   echo "Skipped db_apex_upgrade.py"
